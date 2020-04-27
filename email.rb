1e5e0fc380c187245ece49ec2135f1dfe0f90118
eduardosasso <eduardo.sasso@gmail.com>
2020-04-27 15:51:33 -0700

    Replacing SendGrid with Mailerlite

--- email.rb
+++ email.rb
@@ -2,7 +2,8 @@
 
 require 'net/http'
 require 'dotenv'
-require 'date'
+require 'faraday'
+require 'json'
 
 # https://sendgrid.com/docs/api-reference/
 # prepare and send emails using sendgrid
@@ -17,86 +18,71 @@ class Email
   end
 
   def post
-    update(subject, body)
+    id = create_campaign
 
-    id = create_id
-
-    path = [
-      '/marketing',
-      'singlesends',
-      id,
-      'schedule'
-    ].join('/')
-
-    data = { 'send_at': 'now' }
-
-    request(path, data, 'PUT')
+    update_campaign(id)
+    send_campaign(id)
   end
 
-  def update(subject, body)
-    path = [
-      '/templates',
-      ENV['TEMPLATE_ID'],
-      'versions',
-      ENV['TEMPLATE_VERSION_ID']
-    ].join('/')
-
-    data = {
-      "subject": subject,
-      "html_content": body
-    }
-
-    request(path, data, 'PATCH')
-  end
-
-  def create_id(test = !!ENV['TEST_USER_LIST'])
-    path = '/marketing/singlesends'
-
-    data = {
-      'name': 'Bullish for ' + Date.today.strftime('%A'),
-      'template_id': ENV['TEMPLATE_ID'],
-      'sender_id': ENV['SENDER_ID'].to_i,
-      'filter': { 'send_to_all': !test },
-      'suppression_group_id': ENV['UNSUBSCRIBE_GROUP_ID'].to_i
+  def create_campaign(test = !!ENV['TEST_GROUP'])
+    body = {
+      subject: subject,
+      type: 'regular',
+      groups: ENV['MAIN_GROUP']
     }
 
     if test
-      data[:name] += ' **TEST**'
-      data[:filter] = { 'list_ids': [ENV['TEST_USER_LIST']] }
+      body[:subject] += ' **TEST**'
+      body[:groups] = ENV['TEST_GROUP']
     end
 
-    http = request(path, data, 'POST')
-
-    # returns new singlesend id
-    JSON.parse(http.read_body).dig('id')
+    post_request('/campaigns', body).dig('id')
   end
 
-  def uri
-    URI(ENV['SENDGRID_API'])
-  end
-
-  def http
-    Net::HTTP.new(uri.host, uri.port).tap do |h|
-      h.use_ssl = true
-    end
-  end
-
-  def headers
-    {
-      'Authorization': 'Bearer ' + ENV['SENDGRID_API_KEY'],
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
+  def update_campaign(campaign_id)
+    content = {
+      html: body,
+      plain: '{$unsubscribe} {$url}'
     }
+
+    put_request("/campaigns/#{campaign_id}/content", content)
+  end
+  
+  def send_campaign(campaign_id)
+    post_request("campaigns/#{campaign_id}/actions/send")
   end
 
-  def request(path, data = nil, method = 'GET')
-    data = data.to_json if data
-    path = ENV['API_VERSION'] + path
+  def request(path, data = {}, method = 'get')
+    path = File.join('/api/v2/', path)
 
-    http.send_request(method, path, data, headers).tap do |res|
-      unless %w[200 201].include?(res.code)
-        raise "#{res.code} - #{path} - #{res.body}"
-      end
+    response = mailerlite.send(method, path) do |r|
+      r.body = data.to_json
+    end.tap do |res|
+      raise "#{res.status} - #{path} - #{res.body}" unless res.status == 200
     end
+
+    JSON.parse(response.body)
+  end
+
+  def mailerlite
+    Faraday.new(
+      url: 'https://api.mailerlite.com/',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-MailerLite-ApiKey': ENV['MAILERLITE_API_KEY']
+      }
+    )
+  end
+
+  def get_request(path, data = {})
+    request(path, data, 'get')
+  end
+
+  def post_request(path, data = {})
+    request(path, data, 'post')
+  end
+
+  def put_request(path, data = {})
+    request(path, data, 'put')
   end
 end
---
+++ test/email_test.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require './email'
+
+class EmailTest < Minitest::Test
+end
