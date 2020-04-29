06cc49311923dcb74d52233b14a400a5120a382b
eduardosasso <eduardo.sasso@gmail.com>
2020-04-28 21:20:17 -0700

    Top gainers and losers

---
+++ test/top_test.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require './top'
+
+class TopTest < Minitest::Test
+  def setup
+    @request_fixture = File.read('./test/top_movers.json')
+  end
+
+  def top_movers(type)
+    top = Top.new
+
+    top.stub(:request, JSON.parse(@request_fixture)) do
+      top.stub(:performance, nil) do
+        movers = top.send(type)
+
+        assert(movers.count > 0)
+        assert(movers.first.ticker)
+        assert(movers.first.name)
+        assert(movers.first.price)
+        assert(movers.first.percent)
+      end
+    end
+  end
+
+  def test_losers
+    top_movers('losers')
+  end
+
+  def test_gainers
+    top_movers('gainers')
+  end
+end
---
+++ top.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'faraday'
+require 'dotenv'
+require 'json'
+require './ticker'
+
+# top gainers and losers
+class Top
+  TYPE = {
+    G: 'gainers',
+    L: 'decliners'
+  }.freeze
+
+  Mover = Struct.new(
+    :ticker,
+    :name,
+    :price,
+    :percent,
+    :performance,
+    keyword_init: true
+  )
+
+  def initialize
+    Dotenv.load
+  end
+
+  def gainers
+    mover(TYPE[:G])
+  end
+
+  def losers
+    mover(TYPE[:L])
+  end
+
+  def mover(type = TYPE[:G])
+    request.dig('data', type, 'instruments').map do |s|
+      Mover.new(
+        ticker: s['ticker'],
+        name: s['name'],
+        price: s['lastPrice'],
+        percent: s['percentChange'],
+        performance: performance(s['ticker'])
+      )
+    end
+  end
+
+  def performance(ticker)
+    Ticker.new(ticker).full_performance
+  end
+
+  def request
+    req = Faraday.get(ENV['TOP_GAINERS_LOSERS_API'])
+
+    JSON.parse(req.body)
+  end
+end
