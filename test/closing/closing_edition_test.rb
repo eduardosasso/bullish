330fb99529d385499eb09aa9a4c43c2320a1fc58
eduardosasso <eduardo.sasso@gmail.com>
2020-05-02 17:00:50 -0700

    fix tests

--- test/closing/closing_edition_test.rb
+++ test/closing/closing_edition_test.rb
@@ -18,16 +18,23 @@ class ClosingEditionTest < Minitest::Test
 
   def test_data
     stubbed_top do
-      data = ClosingEdition.new.data
-
-      assert(data[:date_c])
-      assert(data[:preheader_s])
+      closing_edition = ClosingEdition.new
+      closing_edition.stub(:indexes, {}) do 
+        data = closing_edition.data
+        assert(data[:date_c])
+        assert(data[:preheader_s])
+      end
     end
   end
 
   def test_subject
+    indexes = {:sp500_c=>"-2.81%", :nasdaq_c=>"-3.2%", :dowjones_c=>"-2.55%"}
+
     stubbed_top do
-      assert(ClosingEdition.new.subject)
+      closing_edition = ClosingEdition.new
+      closing_edition.stub(:indexes, indexes) do 
+        assert(closing_edition.subject)
+      end
     end
   end
 
