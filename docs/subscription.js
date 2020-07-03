5a49aa69d215fc6eb30da0f7c7d6bf61bc2c0ddb
eduardosasso <eduardo.sasso@gmail.com>
2020-07-02 20:01:41 -0700

    netlify function

---
+++ docs/subscription.js
@@ -0,0 +1,8 @@
+// functions/hello.js
+exports.handler = async event => {
+  const subject = event.queryStringParameters.name || 'World'
+  return {
+    statusCode: 200,
+    body: `Hello ${subject}!`,
+  }
+}
