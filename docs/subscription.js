4d13dd16827db2716af3596f94dfea076453a40a
eduardosasso <eduardo.sasso@gmail.com>
2020-07-02 20:57:22 -0700

    stripe billing logic

--- docs/subscription.js
+++ docs/subscription.js
@@ -1,8 +1,21 @@
-// functions/hello.js
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}!`,
-  }
-}
+  const subject = event.queryStringParameters.name || "World";
+
+  const stripe = require("stripe")("sk_test_D6RF8TPpIHosXCs0Nk5zzUum");
+
+  (async () => {
+    var session = await stripe.billingPortal.sessions.create({
+      customer: "cus_HZSNNTtJhCIyzp",
+      return_url: "https://bullish.email"
+    });
+
+    return session.url;
+  })().then(url => {
+    return {
+      statusCode: 200,
+      headers: {
+        Location: url
+      }
+    };
+  });
+};
