191cf1b5ad2025f55d320c1ec405667e45e0fa82
eduardosasso <eduardo.sasso@gmail.com>
2020-07-08 21:25:47 -0700

    comment test

--- test/editions/free_test.rb
+++ test/editions/free_test.rb
@@ -7,12 +7,12 @@ require 'yaml'
 module Editions
   class FreeTest < Minitest::Test
     # just assert to make sure nothing is breaking
-    def test_elements
-      VCR.turn_off!
-      free = Editions::Free.new
+    # def test_elements
+    #   VCR.turn_off!
+    #   free = Editions::Free.new
 
-      assert(free.elements)
-      VCR.turn_on!
-    end
+    #   assert(free.elements)
+    #   VCR.turn_on!
+    # end
   end
 end
