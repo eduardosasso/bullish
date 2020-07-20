496560dcb212f6005beda0092d73f6ac4d4c20f8
eduardosasso <eduardo.sasso@gmail.com>
2020-07-20 09:40:23 -0700

    comment test

--- test/editions/free_test.rb
+++ test/editions/free_test.rb
@@ -7,11 +7,11 @@ require 'yaml'
 module Editions
   class FreeTest < Minitest::Test
     # just assert to make sure nothing is breaking
-    def test_elements
-      VCR.use_cassette('free_edition_elements', :match_requests_on => [:method]) do
-        free = Editions::Free.new
-        assert(free.elements)
-      end
-    end
+    # def test_elements
+    #   VCR.use_cassette('free_edition_elements', :match_requests_on => [:method]) do
+    #     free = Editions::Free.new
+    #     assert(free.elements)
+    #   end
+    # end
   end
 end
