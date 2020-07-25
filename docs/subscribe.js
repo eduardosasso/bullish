17267a933620dd92fa2ec64e58a102862290a861
Eduardo Sasso <eduardo.sasso@gmail.com>
2020-07-24 19:38:29 -0700

    New website (#8)
    * new website
    
    * bullets
    
    * cleanup
    
    * subscribe js function
    
    * more cleanup
    
    * fix sample email location
    
    * subscribe debug
    
    * footer links, sample badge
    
    * terms and privacy
    
    * subscribe form
    
    * more tweaks
    
    * no zoom on mobile
    
    * remove unused css
    
    * acessibility
    
    * alt
    
    * prevent zoom
    
    * scrolling

---
+++ docs/subscribe.js
@@ -0,0 +1,53 @@
+exports.handler = async event => {
+  const key = process.env["MAILERLITE_API_KEY"];
+  const request = require("axios");
+
+  const Sentry = require("@sentry/node");
+
+  Sentry.init({
+    dsn: process.env["SENTRY_DSN"]
+  });
+
+  const body = JSON.parse(event.body);
+
+  try {
+
+    let option = {
+      method: "POST",
+      url: "https://sapi.mailerlite.com/api/v2/subscribers",
+      data: {
+        email: body.email
+      },
+      headers: {
+        "Content-Type": "application/json",
+        "x-mailerlite-apikey": key
+      }
+    };
+
+    // create subscriber
+    await request(options);
+
+    // free group
+    groupUrl = "https://api.mailerlite.com/api/v2/groups/102872974/subscribers";
+
+    options.url = groupUrl;
+
+    let response = await request(options);
+
+    console.info("SUCCESS: " + body.email);
+
+    return {
+      statusCode: 200
+    };
+
+  } catch (error) {
+    console.error("ERROR: " + body.email);
+
+    Sentry.captureException(error);
+    await Sentry.flush(2000);
+
+    return {
+      statusCode: 500
+    };
+  }
+};
