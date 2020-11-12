ad8d8823fa812bfad63189cc1f720760b7e4522a
eduardosasso <eduardo.sasso@gmail.com>
2020-11-11 20:31:50 -0800

    fix test

--- test/services/news/db_test.rb
+++ test/services/news/db_test.rb
@@ -5,7 +5,7 @@ module Services::News
   class DBTest < Minitest::Test
     def test_find
       VCR.use_cassette('news_stock_find') do
-        item = Services::News::DB.find('PINS')
+        item = Services::News::DB.find('PLTR')
         assert_equal('Services::News::DB::Item', item.class.to_s)
       end
     end
