e1c6d63f1b53f5d24d3d98c5200a88e8298858db
eduardosasso <eduardo.sasso@gmail.com>
2023-01-19 21:11:33 -0800

    add re-captcha to avoid spam

--- site/assets/js/custom.js
+++ site/assets/js/custom.js
@@ -1,6 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("#chimp-form").validate({
-    submitHandler: function(form) {
+    submitHandler: function (form) {
+      if (grecaptcha.getResponse() === "") {
+        $("#response").css("visibility", "visible");
+        $("#response").text("Please verify that you are not a robot.");
+
+        return false;
+      }
+
       $("#response").css("visibility", "hidden");
 
       $("#subscribe-button")
@@ -8,14 +15,14 @@ $(document).ready(function() {
         .prop("disabled", true);
 
       $.post(form.action, JSON.stringify({ email: $("#chimp-email").val() }))
-        .done(function() {
+        .done(function () {
           $("#subscribe-button").attr("value", "Done");
 
           $("#chimp-email").replaceWith(
             "<div id='success'>Thank you! 🎉</div>"
           );
         })
-        .fail(function() {
+        .fail(function () {
           $("#response").css("visibility", "visible");
           $("#response").text("Sorry but something went wrong!");
         });
@@ -23,8 +30,17 @@ $(document).ready(function() {
     rules: {
       email: {
         required: true,
-        email: true
-      }
-    }
+        email: true,
+      },
+      hiddenRecaptcha: {
+        required: function () {
+          if (grecaptcha.getResponse() == "") {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+    },
   });
 });
